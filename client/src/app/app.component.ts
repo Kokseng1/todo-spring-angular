81a65d6ac9a4c75005076d24bc55f54e70927072
kokseng1 <ngkokseng02@gmail.com>
2024-11-15 01:42:39 +0800

    add : spring security

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskListComponent } from './task-list/task-list.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TaskListComponent, HttpClientModule],
+  imports: [RouterOutlet, TaskListComponent, HttpClientModule, BrowserModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  // greeting = {};
+  // constructor(private http: HttpClient) {
+  //   http.get('resource').subscribe((data) => (this.greeting = data));
+  // }
+
   title = 'client';
 }
